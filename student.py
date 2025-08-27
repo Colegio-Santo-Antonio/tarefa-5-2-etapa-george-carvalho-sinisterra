f785d8d6420c3499bb5034bd1f462a76d8b00627
Gabrielpardini-sudo <csa23-1790@colegiosantoantonio.com.br>
2025-08-27 17:00:55 -0300

    Update student.py

--- student.py
+++ student.py
@@ -2,13 +2,13 @@ numero = input()
 impares = []
 for i in numeros[ -1::2] :
   impares.append(int(1))
-  pares = []
-  for i in numeros[-2::-2]:
+pares = []
+for i in numeros[-2::-2]:
     if 2*int(i)<10
     pares.append(2*int(i))
 else:
-  pares.append(2*int(i) - 10+1)
-  somatorio= sun(pares) + sun (impares)
+   pares.append(2*int(i) - 10+1)
+  some = sun(pares) + sun (impares)
   if int(somatorio)/10) == somatorio/10:
   print("Cartão valido") 
 else:
